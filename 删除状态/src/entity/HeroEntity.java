70523b2c78c33664d6973f765fb1d6ec56a45a77
y1笑而过 <15335747148@163.com>
2018-06-25 20:09:43 +0800

    删除状态
    删除状态

---
+++ 删除状态/src/entity/HeroEntity.java
@@ -0,0 +1,106 @@
+package entity;
+
+import javax.persistence.*;
+import java.math.BigInteger;
+import java.sql.Timestamp;
+import java.util.Objects;
+
+/**
+ * -- auto-generated definition
+ * create table hero
+ * (
+ *   id           bigint unsigned auto_increment,
+ *   gmt_create   datetime    not null,
+ *   gmt_modified datetime    not null,
+ *   name         varchar(20) not null,
+ *   constraint hero_id_uindex
+ *   unique (id)
+ * );
+ *
+ * alter table hero
+ *   add primary key (id);
+ */
+@Entity
+@Table(name = "hero", schema = "lab", catalog = "lab")
+public class HeroEntity {
+    private BigInteger id;
+    private Timestamp gmtCreate;
+    private Timestamp gmtModified;
+    private String name;
+
+    public HeroEntity() {
+    }
+
+    public HeroEntity( Timestamp gmtCreate, Timestamp gmtModified, String name) {
+        this.gmtCreate = gmtCreate;
+        this.gmtModified = gmtModified;
+        this.name = name;
+    }
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public BigInteger getId() {
+        return id;
+    }
+
+    public void setId(BigInteger id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "gmt_create", nullable = false)
+    public Timestamp getGmtCreate() {
+        return gmtCreate;
+    }
+
+    public void setGmtCreate(Timestamp gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
+
+    @Basic
+    @Column(name = "gmt_modified", nullable = false)
+    public Timestamp getGmtModified() {
+        return gmtModified;
+    }
+
+    public void setGmtModified(Timestamp gmtModified) {
+        this.gmtModified = gmtModified;
+    }
+
+    @Basic
+    @Column(name = "name", nullable = false, length = 20)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        HeroEntity that = (HeroEntity) o;
+        return id == that.id &&
+                Objects.equals(gmtCreate, that.gmtCreate) &&
+                Objects.equals(gmtModified, that.gmtModified) &&
+                Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, gmtCreate, gmtModified, name);
+    }
+
+    @Override
+    public String toString() {
+        return "HeroEntity{" +
+                "id=" + id +
+                ", gmtCreate=" + gmtCreate +
+                ", gmtModified=" + gmtModified +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
